36d17796877fbe46276dc49febad29b37213ba90
harshvardhan2014e-rgb <harshvardhan2014.e@gmail.com>
2026-05-28 20:42:46 +0530

    Data Types

---
+++ Data Types.py
@@ -0,0 +1,22 @@
+name = "Harsh"
+age = 12
+is_student = True
+weight = 39.5
+
+print("Name :", name)
+print("Data Type of name is", type(name))
+print("Age :", age)
+print("Data types Of Age is", type(age))
+print("is_student :", is_student)
+print("Data Type of its is_student is", type(is_student))
+print("Weight :", weight)
+print("Data Type of weight is", type(weight))
+
+print("\n After Type Casting....")
+age = str(age)
+print(age)
+print("Data Type of age is", type(age))
+weight = int(weight)
+print(weight)
+print("Data Type of weight is", type(weight))
+
