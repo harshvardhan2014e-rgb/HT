f6c4bf6f73370375b22cdc74b1545067eaa2a245
harshvardhan2014e-rgb <harshvardhan2014.e@gmail.com>
2026-06-04 20:42:24 +0530

    Turtle3

---
+++ Turtle2.py
@@ -0,0 +1,28 @@
+import turtle
+
+turtle.Screen().bgcolor("Green")
+
+board = turtle.Turtle()
+
+#Triangle
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+board.left(120)
+board.forward(100)
+
+
+#square
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
+
+board.forward(100)
+board.left(90)
