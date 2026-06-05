cae60ce64211a56d287d94a06a8498c4b2201c28
harshvardhan2014e-rgb <harshvardhan2014.e@gmail.com>
2026-06-05 11:34:59 +0530

    Turtle Square And Rectangle

---
+++ Turtles Shapes.py
@@ -0,0 +1,30 @@
+import turtle
+
+turtle.Screen().bgcolor("#08bfad")
+board = turtle.Turtle()
+
+pen = turtle.Turtle()
+
+# Draw a square
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+pen.right(90)
+pen.forward(100)
+pen.right(90)
+
+# Draw a rectangle
+pen.forward(150)
+pen.right(90)
+pen.forward(80)
+pen.right(90)
+pen.forward(150)
+pen.right(90)
+pen.forward(80)
+pen.right(90)
+
+turtle.done()
+
+
