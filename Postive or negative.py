ece2de63f06f71485f68f5f1f0680f5ef20ec8b9
harshvardhan2014e-rgb <harshvardhan2014.e@gmail.com>
2026-06-09 20:17:50 +0530

    Postive or negative

---
+++ Postive or negative.py
@@ -0,0 +1,11 @@
+number = int(input("Enter a number: "))
+print("number to be checked is: ", number)
+
+if number > 0:
+    print("The number is positive.")
+
+elif number < 0:
+    print("The number is negative.")
+
+else:
+    print("The number is neutral.")
