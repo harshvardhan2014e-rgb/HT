34088bfc96d0d77820d51e0ce5238374d63213fd
harshvardhan2014e-rgb <harshvardhan2014.e@gmail.com>
2026-06-16 20:42:40 +0530

    Function

---
+++ Fib-Calculator/Function.py
@@ -0,0 +1,11 @@
+def absolute_value(num):
+
+    if num < 0:
+        return -num
+    else:
+        return num
+    
+
+print("absolute_value of 89: ", absolute_value(89))
+
+print("absolute_value of -189: ", absolute_value(-189))
