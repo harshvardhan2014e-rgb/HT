4bf7f0a4d0758caf772f78ca5de36091394486dc
harshvardhan2014e-rgb <harshvardhan2014.e@gmail.com>
2026-06-24 15:14:20 +0530

    OOP2

---
+++ OOP2.py
@@ -0,0 +1,9 @@
+# Circle area and perimeter 
+
+radius = float(input("Enter radius: "))
+
+area = (22/7) * radius * radius
+perimeter = 2 * (22/7) * radius
+
+print("Area:", area)
+print("Perimeter:", perimeter)
