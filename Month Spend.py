67d5d0e2b39df57e2712f6c437aa9dd2888f4b1c
harshvardhan2014e-rgb <harshvardhan2014.e@gmail.com>
2026-07-08 18:41:19 +0530

    Spend

---
+++ Month Spend.py
@@ -0,0 +1,23 @@
+amount = int(input(" Enter the amount: "))
+rent = int(input(" Enter the rent: "))
+
+
+print(" Total Left:", amount-rent)
+
+Leftover = amount-rent
+
+Shoping = int(input("Enter The Shoping Amount : "))
+
+print(" Total Left After Shoping :", Leftover-Shoping)
+
+Leftover2 = Leftover-Shoping
+
+EMI = int(input("Enter The EMI: "))
+
+print(" Total Left After EMI: ", Leftover2-EMI)
+
+
+
+
+
+
