d7da199955b58b9910b9c8c486c49fba88e55e34
harshvardhan2014e-rgb <harshvardhan2014.e@gmail.com>
2026-07-11 13:36:10 +0530

    Login_Medicore-HMS

---
+++ Test Bug.py
@@ -0,0 +1,11 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+if num1 == num2:
+    print("Numbers are equal")
+
+answer = num1 / num2
+
+print("Answer is:", answer)
+
+if num
