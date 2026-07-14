fb2bb09b054515df00f4a83e73f1ddc9ac8029b5
harshvardhan2014e-rgb <harshvardhan2014.e@gmail.com>
2026-07-14 20:55:12 +0530

    FindHowManyLines

---
+++ Codingal/Howmanylines.py
@@ -0,0 +1,12 @@
+file = open("Codingal.txt","r")
+Counter = 0
+
+Content = file.read()
+Colist = Content.split("\n")
+
+for i in Colist:
+    if i:
+        Counter += 1
+
+print("This is the number of lines in the file: ")
+print(Counter)
